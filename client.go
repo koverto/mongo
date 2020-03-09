6e6bde0b7ef954925829e56a273b9aedc5cbc70a
Jesse B. Hannah <jesse@jbhannah.net>
2020-03-09 09:26:56 -0700

    Documentation and linting fixes

--- client.go
+++ client.go
@@ -1,3 +1,5 @@
+// Package mongo provides a simplified wrapper for mongo.Client from the
+// official MongoDB library.
 package mongo
 
 import (
@@ -8,6 +10,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const timeout = 10 * time.Second
+
+// Client is the wrapper interface for mongo.Client, containing a connection to
+// a single database on a server.
 type Client interface {
 	Collection(string, ...*options.CollectionOptions) *mongo.Collection
 	Connect() error
@@ -21,6 +27,8 @@ type client struct {
 	indexSets []IndexSet
 }
 
+// NewClient creates a new Client for the MongoDB server at uri and database
+// dbName.
 func NewClient(uri string, dbName string) (Client, error) {
 	c, err := mongo.NewClient(options.Client().ApplyURI(uri))
 	if err != nil {
@@ -33,19 +41,23 @@ func NewClient(uri string, dbName string) (Client, error) {
 	}, nil
 }
 
+// Collection returns the collection at name with options.CollectionOptions opts
+// for the connected database.
 func (c *client) Collection(name string, opts ...*options.CollectionOptions) *mongo.Collection {
 	return c.Database().Collection(name, opts...)
 }
 
+// Connect opens the connection to the database, waits for the server to respond
+// to ping, and creates the indexes.
 func (c *client) Connect() error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
 	if err := c.Client.Connect(ctx); err != nil {
 		return err
 	}
 
-	pingCtx, cancelPing := context.WithTimeout(context.Background(), 10*time.Second)
+	pingCtx, cancelPing := context.WithTimeout(context.Background(), timeout)
 	defer cancelPing()
 
 	if err := c.Client.Ping(pingCtx, nil); err != nil {
@@ -59,16 +71,18 @@ func (c *client) Connect() error {
 	return nil
 }
 
+// Database returns the database for the client.
 func (c *client) Database() *mongo.Database {
 	return c.Client.Database(c.name)
 }
 
+// DefineIndexes appends the given indexSets to the Client.
 func (c *client) DefineIndexes(indexSets ...IndexSet) {
 	c.indexSets = append(c.indexSets, indexSets...)
 }
 
 func (c *client) createIndexes() error {
-	createOpts := options.CreateIndexes().SetMaxTime(10 * time.Second)
+	createOpts := options.CreateIndexes().SetMaxTime(timeout)
 
 	for _, indexSet := range c.indexSets {
 		iv := c.Collection(indexSet.Collection).Indexes()
--- index_set.go
+++ index_set.go
@@ -2,11 +2,14 @@ package mongo
 
 import "go.mongodb.org/mongo-driver/mongo"
 
+// IndexSet defines a set of indexes to create on a MongoDB collection.
 type IndexSet struct {
 	Collection string
 	Indexes    []mongo.IndexModel
 }
 
+// NewIndexSet creates a new IndexSet for the given collection containing the
+// given indexes.
 func NewIndexSet(collection string, indexes ...mongo.IndexModel) IndexSet {
 	return IndexSet{
 		Collection: collection,
